95e690543c9016d07fe8271240f7f7e09dfc5c94
JasonOldWoo <JasonOldWoo@users.noreply.github.com>
2014-02-28 11:07:10 +0800

    Create test.cpp
    test code

---
+++ DArrStack/test.cpp
@@ -0,0 +1,14 @@
+#include "darr_stack.h"
+#define BUF_SIZE 1024
+
+int main(int argc, char *argv)
+{
+	DArrStack<char> arr_stk;
+	char str[BUF_SIZE] = "Hello, world!";
+	arr_stk.push_back(str, strlen(str));
+	std::cout << arr_stk.GetTop() << std::endl;
+	char *arr = arr_stk.pop();
+	std::cout << arr << std::endl;
+	delete []arr;
+	return 0;
+}
